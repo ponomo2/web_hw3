432961d1f0234aa0f9bcb335ce966305184821c1
ponomo2 <ponom.sofia03@gmail.com>
2023-11-14 01:13:12 +0300

    added index page with generated info and pagination

--- askme/app/views.py
+++ askme/app/views.py
@@ -1,8 +1,31 @@
+import random
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'base.html')
+    qattributes = []
+    for i in range(1, 30):
+        qattributes.append({
+            'title': 'title' + str(i),
+            'id': i,
+            'text': 'text' + str(i),
+            'tags': ['one'+str(i), 'two'+str(i), 'three'+str(i)],
+            'answerNum': random.randint(2, 7),
+            'dislikes': random.randint(2, 10),
+            'likes': random.randint(2, 20)
+        })
+
+
+    paginator = Paginator(qattributes,10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    data = {
+        'tags': ['first', 'second', 'third'],
+        'members': ['member1', 'member2', 'member3'],
+        'page_obj': page_obj
+    }
+    return render(request, 'index.html', data)
 
 def question(request):
     return render(request, 'base.html')
