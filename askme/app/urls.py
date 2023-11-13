1f3ece4fe54a59eff1f284d04b01d8a40b21ad69
ponomo2 <ponom.sofia03@gmail.com>
2023-11-13 08:50:27 +0300

    added urls and static bootstrap4

--- askme/app/urls.py
+++ askme/app/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name="mainQP"),
+    path('hot', views.index, name="hotQP"),
+    path('tag/blablabla', views.tag, name="tagQP"),
+# cтраница одного вопроса со списком ответов (URL = /question/35/)
+#     path('', views.index, name="main"),
+    path('login', views.login, name="loginP"),
+    path('signup', views.signup, name="signupP"),
+    path('ask', views.ask, name="newQP"),
+    path('profile', views.settings, name="settingsP")
 ]
--- askme/app/views.py
+++ askme/app/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+
+def tag(request):
+    return render(request, 'base.html')
+
+def login(request):
+    return render(request, 'base.html')
+
+def signup(request):
+    return render(request, 'base.html')
+
+def ask(request):
+    return render(request, 'base.html')
+
+def settings(request):
+    return render(request, 'base.html')
