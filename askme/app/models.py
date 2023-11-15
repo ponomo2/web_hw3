98ed9f4cdaf12e185f7eb2ee370b133e9553e3fb
ponomo2 <ponom.sofia03@gmail.com>
2023-11-15 18:31:57 +0300

    added tables without managers

--- askme/app/models.py
+++ askme/app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    tag_id = models.IntegerField(primary_key=True)
+    text = models.CharField(max_length=50)
+
+
+class Profile(models.Model):
+    avatar = models.ImageField
+    nickname = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+
+class Likes(models.Model):
+    likes_number = models.IntegerField(default=0)
+    dislikes_number = models.IntegerField(default=0)
+
+
+class Question(models.Model):
+    question_id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=1000)
+    likes = models.ForeignKey(Likes, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+
+class Answer(models.Model):
+    answer_id = models.IntegerField(primary_key=True)
+    text = models.CharField(max_length=1000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ForeignKey(Likes, on_delete=models.PROTECT)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
